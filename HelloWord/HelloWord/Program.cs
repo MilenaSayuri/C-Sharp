dd1c76b603b35a4f1058545fade2bdc180b21099
Milena Sayuri Hayakawa <mirenahayakawa@gmail.com>
2025-02-21 21:26:05 -0300

    o comeco e basico de c#

---
+++ HelloWord/HelloWord/Program.cs
@@ -0,0 +1,20 @@
+﻿//mótodos ou pacotes
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+            Console.WriteLine("Meu nome eh Milena");
+
+            Console.ReadLine();
+        }
+    }
+}
